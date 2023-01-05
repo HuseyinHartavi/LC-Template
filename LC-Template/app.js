2d8d64fc495d9fd6268f5b8ff8a48aab287a6ebb
Huseyin Hartavi <huseyinhartavi35@gmail.com>
2023-01-05 23:45:57 +0300

    LC-Template
    I made this project with vanilla VS Code.

---
+++ LC-Template/app.js
@@ -0,0 +1,12 @@
+function darkmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
+function lightmode() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+}
+
+
+
